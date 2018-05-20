905b816a6958b878f18c4c9929a88512a4d3d2f2
Empiah <phippsoli@hotmail.com>
2018-05-20 15:27:41 +0100

    updated to handle input better and produce graphs

--- LCS_UInput.py
+++ LCS_UInput.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 def get_time_period():
     period_options = ['month', 'year', 'none']
     month_options = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
-    year_options = ['2016', '2017']
+    year_options = [2016, 2017]
 
-    print('Hello, in this script you will be able to get details about London stop and search data!')
+    print('\nHello, in this script you will be able to get details about London stop and search data!')
     print('\nFirst thing that we want to do is see what time period you want to view the information for.')
     print('\nBy default we will range from January 2016 to April 2017, but you have the option to filter the data')
 
@@ -44,15 +44,15 @@ def get_time_period():
     elif period == 'year':
         while True:
             try:
-                year_input = str(input('\nSo you want to search by year (remember we only have a few month for 2017), what year shall we search on?'))
+                year_input = int(input('\nSo you want to search by year (remember we only have a few month for 2017), what year shall we search on?'))
             except ValueError:
                 print('{} that is not a legitimate thing to enter'.format(year_input))
                 continue
-            if year_input.lower() not in year_options:
+            if year_input not in year_options:
                 print('{} that was not a year on the list, its 2016 or 2017...'.format(year_input))
                 continue
             else:
-                year = year_input.lower()
+                year = year_input
                 month = 'all'
                 break
     #if they dont select month or year than we need to set those variables to show all
@@ -66,7 +66,7 @@ def get_time_period():
 #if we filter the data it becomes so much easier to do analysis on
 def load_data(period, month, year):
     #load the dataset from Kaggle (https://www.kaggle.com/sohier/london-police-records)
-    df = pd.read_csv('/Users/oliverphipps/Dropbox/6. Python/Python Projects/LondonCrimeStatistics/london-stop-and-search.csv')
+    df = pd.read_csv('/Users/oliverphipps/Dropbox/6. Python/Python Projects/LondonCrimeStatistics/london-stop-and-search.csv', low_memory=False)
 
     #as I have done analysis on this already via Jupyter notebooks I know that I have to clean this data
     #renaming columns to something more applicable
@@ -116,21 +116,23 @@ def total_search_breakdown(df_clean):
 
     print('\nThe below plot shows the count of total stop and search')
 
-    plt.figure(figsize=(20,10))
+    plt.figure(figsize=(10,7))
     plt.xlabel('Date')
     plt.ylabel('Count of Stop and Search Record')
     plt.title('Time series graph showing stop and search activity')
-    plt.plot(count_by_date);
+    plt.plot(count_by_date)
+    plt.show()
 
 def ethnicity_breakdown(df_clean):
     print('\nThe below plot shows the count of stop and search by ethnicity')
 
-    df_clean['ethnicity'].value_counts().plot.bar(title='stop_and_search_by_ethnicity',figsize=(15,10));
+    df_clean['ethnicity'].value_counts().plot.bar(title='stop_and_search_by_ethnicity',figsize=(10,7))
+    plt.show()
 
 def objective_breakdown(df_clean):
     print('\nThe below shoes the count of stop and search by objective')
 
-    df_clean['object_of_search'].value_counts().plot.bar(title='Stop and Search by Object of Search',figsize=(15,10));
+    df_clean['object_of_search'].value_counts().plot.bar(title='Stop and Search by Object of Search',figsize=(10,7));
 
     print('\nThe below groups "Controlled Drugs" and "Articles for use in Criminal Damage" and shows them as a percentage of each other')
     counts = df_clean['object_of_search'].value_counts()
@@ -153,20 +155,23 @@ def objective_breakdown(df_clean):
     explode = (0,0)
     plt.axis("equal")
     plt.title('Percentage Drug vs. Weapons search as pct of Drugs plus Weapons')
-    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True);
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True)
+    plt.show()
 
 def eth_objective_breakdown(df_clean):
     print('\nThe below groups ethnicity and obective breakdowns together to try and show us some trends')
 
     print('\nFirst by objective of search and then by ethnicity')
-    df_clean.groupby('ethnicity')['object_of_search'].value_counts().unstack(0).plot.bar(title='Object of search by Ethnicity', figsize=(15,10));
+    df_clean.groupby('ethnicity')['object_of_search'].value_counts().unstack(0).plot.bar(title='Object of search by Ethnicity', figsize=(10,7))
+    plt.show()
 
     print('\nAnd now by ethnicity and then objective of search')
-    df_clean.groupby('object_of_search')['ethnicity'].value_counts().unstack(0).plot.bar(title='Ethnicity by Object of Search', figsize=(15,10));
+    df_clean.groupby('object_of_search')['ethnicity'].value_counts().unstack(0).plot.bar(title='Ethnicity by Object of Search', figsize=(10,7))
+    plt.show()
 
 def gender_breakdown(df_clean):
     print('\nThis here will show us a bar chart of Gender by the Objective of search')
-    df_clean.groupby('object_of_search')['gender'].value_counts().unstack(0).plot.bar(title='Gender by Object of Search', figsize=(15,10));
+    df_clean.groupby('object_of_search')['gender'].value_counts().unstack(0).plot.bar(title='Gender by Object of Search', figsize=(10,7));
 
     print('\nNow lets see that total stop and search counts by Gender')
 
@@ -186,7 +191,8 @@ def gender_breakdown(df_clean):
     explode = (0.0,0,0)
     plt.axis("equal")
     plt.title('Percentage of Stop and Search by Gender')
-    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True);
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True)
+    plt.show()
 
 def age_breakdown(df_clean):
     print('\nNow lets see a gender breakdown by count as percentage of total')
@@ -211,10 +217,12 @@ def age_breakdown(df_clean):
     explode = (0,0,0,0,0)
     plt.axis("equal")
     plt.title('Percentage of Stop and Search by Age range')
-    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True);
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True)
+    plt.show()
 
     print('\nNow lets analyse this more by looking at age by Objective of search')
-    df_clean.groupby('object_of_search')['age_range'].value_counts().unstack(0).plot.bar(title='Age range by Object of Search', figsize=(15,10));
+    df_clean.groupby('object_of_search')['age_range'].value_counts().unstack(0).plot.bar(title='Age range by Object of Search', figsize=(10,7))
+    plt.show()
 
 def time_series_graphs(df_clean):
     print('\nWe have looked at top level statistics now, but what is useful is to see how things change over time')
@@ -224,22 +232,26 @@ def time_series_graphs(df_clean):
     #first by ethnicity
     race_count = df_clean.groupby('date')
     race_count = race_count.ethnicity.apply(pd.value_counts).unstack(-1).fillna(0)
-    race_count.plot(kind='line',figsize=(20,10), title='Stop and Search Count by Race');
+    race_count.plot(kind='line',figsize=(10,7), title='Stop and Search Count by Race')
+    plt.show()
 
     print('\nNow by objective of search')
     object_count = df_clean.groupby('date')
     object_count = object_count.object_of_search.apply(pd.value_counts).unstack(-1).fillna(0)
-    object_count.plot(kind='line',figsize=(20,10), title='Stop and Search Count by Object of Search');
+    object_count.plot(kind='line',figsize=(10,7), title='Stop and Search Count by Object of Search')
+    plt.show()
 
     print('\nAnd by Gender')
     gender_count = df_clean.groupby('date')
     gender_count = gender_count.gender.apply(pd.value_counts).unstack(-1).fillna(0)
-    gender_count.plot(kind='line',figsize=(20,10), title='Stop and Search Count by Gender');
+    gender_count.plot(kind='line',figsize=(10,7), title='Stop and Search Count by Gender')
+    plt.show()
 
     print('\nLastly, age')
     age_count = df_clean.groupby('date')
     age_count = age_count.age_range.apply(pd.value_counts).unstack(-1).fillna(0)
-    age_count.plot(kind='line', figsize=(20,10), title='Stop and Search Count by Age');
+    age_count.plot(kind='line', figsize=(10,7), title='Stop and Search Count by Age')
+    plt.show()
 
 def random_line(df_clean):
     # this is used to select a random line of data to look at
