e0ce21a842d98987d876fcac8e07ec51bc3e4585
Empiah <phippsoli@hotmail.com>
2018-05-20 14:37:13 +0100

    added age line graph

--- LCS_UInput.py
+++ LCS_UInput.py
@@ -109,3 +109,134 @@ def load_data(period, month, year):
         df_clean = df_clean[df_clean['year'] == year]
 
     return df_clean
+
+#from here on in are analysis functions which will show us relevant data
+def total_search_breakdown(df_clean):
+    count_by_date = df_clean.groupby('date').size()
+
+    print('\nThe below plot shows the count of total stop and search')
+
+    plt.figure(figsize=(20,10))
+    plt.xlabel('Date')
+    plt.ylabel('Count of Stop and Search Record')
+    plt.title('Time series graph showing stop and search activity')
+    plt.plot(count_by_date);
+
+def ethnicity_breakdown(df_clean):
+    print('\nThe below plot shows the count of stop and search by ethnicity')
+
+    df_clean['ethnicity'].value_counts().plot.bar(title='stop_and_search_by_ethnicity',figsize=(15,10));
+
+def objective_breakdown(df_clean):
+    print('\nThe below shoes the count of stop and search by objective')
+
+    df_clean['object_of_search'].value_counts().plot.bar(title='Stop and Search by Object of Search',figsize=(15,10));
+
+    print('\nThe below groups "Controlled Drugs" and "Articles for use in Criminal Damage" and shows them as a percentage of each other')
+    counts = df_clean['object_of_search'].value_counts()
+    counts[counts > 5000]
+
+    #creating some masks for the two we are ooking for
+    drugs = df_clean.object_of_search == 'Controlled drugs'
+    criminal_damage = df_clean.object_of_search == 'Articles for use in criminal damage'
+
+    #creating a total that we can use as denominator for the percentage calculator
+    drugs_p_crimdam = df_clean.object_of_search[criminal_damage].count() + df_clean.object_of_search[criminal_damage].count()
+
+    #creating some percentages for the pie chart
+    drugs_prc = df_clean.object_of_search[drugs].count() / drugs_p_crimdam
+    criminal_damage_prc = df_clean.object_of_search[criminal_damage].count() / drugs_p_crimdam
+
+    # this will plot a pie chart for us using the above
+    labels = 'Controlled Drugs', 'Articles for use in criminal damage'
+    fracs = [drugs_prc, criminal_damage_prc]
+    explode = (0,0)
+    plt.axis("equal")
+    plt.title('Percentage Drug vs. Weapons search as pct of Drugs plus Weapons')
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True);
+
+def eth_objective_breakdown(df_clean):
+    print('\nThe below groups ethnicity and obective breakdowns together to try and show us some trends')
+
+    print('\nFirst by objective of search and then by ethnicity')
+    df_clean.groupby('ethnicity')['object_of_search'].value_counts().unstack(0).plot.bar(title='Object of search by Ethnicity', figsize=(15,10));
+
+    print('\nAnd now by ethnicity and then objective of search')
+    df_clean.groupby('object_of_search')['ethnicity'].value_counts().unstack(0).plot.bar(title='Ethnicity by Object of Search', figsize=(15,10));
+
+def gender_breakdown(df_clean):
+    print('\nThis here will show us a bar chart of Gender by the Objective of search')
+    df_clean.groupby('object_of_search')['gender'].value_counts().unstack(0).plot.bar(title='Gender by Object of Search', figsize=(15,10));
+
+    print('\nNow lets see that total stop and search counts by Gender')
+
+    #creating some masks
+    gender_male = df_clean.gender == 'Male'
+    gender_female = df_clean.gender == 'Female'
+    gender_other = df_clean.gender == 'Other'
+
+    #creating percentages
+    gender_male_prc = df_clean.gender[gender_male].count() / len(df_clean)
+    gender_female_prc = df_clean.gender[gender_female].count() / len(df_clean)
+    gender_other_prc = df_clean.gender[gender_other].count() / len(df_clean)
+
+    # this will plot a pie chart for us using the calcs above
+    labels = 'Male', 'Female', 'Other'
+    fracs = [gender_male_prc, gender_female_prc, gender_other_prc]
+    explode = (0.0,0,0)
+    plt.axis("equal")
+    plt.title('Percentage of Stop and Search by Gender')
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True);
+
+def age_breakdown(df_clean):
+    print('\nNow lets see a gender breakdown by count as percentage of total')
+
+    #as above, lets make some masks
+    age_u10 = df_clean.age_range == 'under 10'
+    age_10 = df_clean.age_range == '10-17'
+    age_18 = df_clean.age_range == '18-24'
+    age_25 = df_clean.age_range == '25-34'
+    age_34 = df_clean.age_range == 'over 34'
+
+    #and some percentages
+    age_u10_prc = df_clean.age_range[age_u10].count() / len(df_clean)
+    age_10_prc = df_clean.age_range[age_10].count() / len(df_clean)
+    age_18_prc = df_clean.age_range[age_18].count() / len(df_clean)
+    age_25_prc = df_clean.age_range[age_25].count() / len(df_clean)
+    age_34_prc = df_clean.age_range[age_34].count() / len(df_clean)
+
+     # and a pie chart
+    labels = 'Under 10', '10-17', '18-24', '25-34', '35+'
+    fracs = [age_u10_prc, age_10_prc, age_18_prc, age_25_prc, age_34_prc]
+    explode = (0,0,0,0,0)
+    plt.axis("equal")
+    plt.title('Percentage of Stop and Search by Age range')
+    plt.pie(fracs, explode=explode, labels=labels, autopct='%.0f%%', shadow=True);
+
+    print('\nNow lets analyse this more by looking at age by Objective of search')
+    df_clean.groupby('object_of_search')['age_range'].value_counts().unstack(0).plot.bar(title='Age range by Object of Search', figsize=(15,10));
+
+def time_series_graphs(df_clean):
+    print('\nWe have looked at top level statistics now, but what is useful is to see how things change over time')
+    print('\nFor that lets plot some graphs showing change over time')
+
+    print('\nLets look first at this by Ethnicity')
+    #first by ethnicity
+    race_count = df_clean.groupby('date')
+    race_count = race_count.ethnicity.apply(pd.value_counts).unstack(-1).fillna(0)
+    race_count.plot(kind='line',figsize=(20,10), title='Stop and Search Count by Race');
+
+    print('\nNow by objective of search')
+    object_count = df_clean.groupby('date')
+    object_count = object_count.object_of_search.apply(pd.value_counts).unstack(-1).fillna(0)
+    object_count.plot(kind='line',figsize=(20,10), title='Stop and Search Count by Object of Search');
+
+    print('\nAnd by Gender')
+    gender_count = df_clean.groupby('date')
+    gender_count = gender_count.gender.apply(pd.value_counts).unstack(-1).fillna(0)
+    gender_count.plot(kind='line',figsize=(20,10), title='Stop and Search Count by Gender');
+
+    print('\nLastly, age')
+    age_count = df_clean.groupby('date')
+    age_count = age_count.age_range.apply(pd.value_counts).unstack(-1).fillna(0)
+    age_count.plot(kind='line', figsize=(20,10), title='Stop and Search Count by Age');
